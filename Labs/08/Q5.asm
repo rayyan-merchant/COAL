541542223e31e9d5e3070d90523c91e07db64588
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-04-27 18:13:33 +0500

    Create Q5.asm

---
+++ Labs/08/Q5.asm
@@ -0,0 +1,39 @@
+; Write a program for sequential search. Take an input from the user and find if it occurs in the following array:
+;            arr WORD 10, 4, 7, 14, 299, 156, 3, 19, 29, 300, 20
+
+include irvine32.inc
+
+.data
+	arr WORD 10, 4, 7, 14, 299, 156, 3, 19, 29, 300, 20
+	num WORD ?
+	prompt BYTE "Enter a number: ", 0
+	msg1 BYTE "Found.", 0
+	msg2 BYTE "Not Found", 0
+
+.code
+main PROC
+	mov edx, offset prompt
+	call writestring
+	call readint
+	mov ecx, lengthof arr
+	mov esi, offset arr
+	
+  search_loop:
+		mov ebx, [esi]
+		cmp eax, ebx
+		je found
+		add esi, 2
+		loop search_loop
+	
+  mov edx, offset msg2
+	call writestring
+	jmp end_code
+	
+  found:
+		mov edx, offset msg1
+		call writestring
+	
+  end_code:
+    exit
+main endp
+end main
