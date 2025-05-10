1387cd3e9553bd3bcc834c51fbf6784706591983
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-05-10 16:56:52 +0500

    Update Q1.asm

--- Labs/09/Q1.asm
+++ Labs/09/Q1.asm
@@ -1,1 +1,28 @@
+; 21 = 2^4 + 2^2 + 2^1
 
+INCLUDE Irvine32.inc
+
+.data
+    ;
+.code
+
+main PROC
+    call ReadInt
+    mov ecx, 0
+
+    mov ebx, eax
+    shl ebx, 4
+    add ecx, ebx
+
+    mov ebx, eax
+    shl ebx, 2
+    add ecx, ebx
+
+    add ecx, eax
+
+    mov eax, ecx
+
+    call WriteDec
+    exit
+main ENDP
+END main
